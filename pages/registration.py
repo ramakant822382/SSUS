79e21cb71edee590b78fe7c04b3fc8686d513c7c
Ramakant sharma <102178267+ramakant822382@users.noreply.github.com>
2026-07-16 16:24:44 +0530

    ee

--- pages/registration.py
+++ pages/registration.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from database.mongodb import students_collection
-st.title("---Student Registration----")
+st.title(":red[---Student Registration----]")
 
 first_name = st.text_input("First Name")
 
