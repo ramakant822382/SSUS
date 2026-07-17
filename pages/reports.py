7dc8802e7ed2c4266eaee5248d2b5d1c1cdbf728
Ramakant sharma <102178267+ramakant822382@users.noreply.github.com>
2026-07-17 15:49:15 +0530

    add report file

---
+++ pages/reports.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+import streamlit as st 
+
+from database.mongodb import students_collection
+
+data= list(
+    students_collection.find()
+)
+
+df=pd.DataFrame(data)
+
+st.dataframe(df)
+
+st.bar_chart(
+    df["age"]
+)
